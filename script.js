83fd04a720c19791f3d9112246b30d3cbb78afa6
Tahaberk SOYSAL <berksysl.g@gmail.com>
2024-10-11 16:14:25 +0300

    Make api requests realtime

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const form = document.querySelector('form');
+const amountEl = document.querySelector("input[type='number']");
+const baseCurr = document.querySelector("#baseCurrency");
+const targetCurr = document.querySelector("#targetCurrency");
+const resultEl = document.querySelector("#result");
+const selectEls = document.querySelectorAll('select');
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const baseCurr = document.querySelector("#baseCurrency");
-  const targetCurr = document.querySelector("#targetCurrency");
+amountEl.addEventListener("keyup", () => {
   requestData(baseCurr.value, targetCurr.value);
+});
+
+selectEls.forEach((el) => {
+  el.addEventListener('change', () => requestData(baseCurr.value, targetCurr.value))
 })
+
+form.addEventListener("submit",(e) => e.preventDefault())
 
 async function requestData(baseCurr, targetCurr) {
   const response = await fetch(`https://v6.exchangerate-api.com/v6/640fa2bdc5dd47399a72bdf3/latest/${baseCurr}`);
@@ -14,7 +22,5 @@ async function requestData(baseCurr, targetCurr) {
 }
 
 function displayRateResult(rate) {
-  const amountEl = document.querySelector("input[type='number']");
-  const resultEl = document.querySelector("#result");
-  resultEl.innerText = (rate * amountEl.value).toFixed(2);
+  resultEl.textContent = `${(rate * amountEl.value).toFixed(2)} ${targetCurr.value}`;
 }
